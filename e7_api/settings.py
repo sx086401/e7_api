6100c42c4024f1aaaffef2082300d26b77dc5110
Evan Tseng <zx086401@gmail.com>
2023-07-21 16:52:03 +0800

    add allow hosts

--- e7_api/settings.py
+++ e7_api/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-=g$5hsy)zuu=dnbjtn82%3wvip_sr&hk&u$93a4o-t-hu^83)&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['e7-state-management.zeabur.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
